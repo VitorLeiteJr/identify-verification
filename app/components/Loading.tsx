8d44ef97c05bb141f3c7415d3386e1eb9062c2ee
Vitor Leite <134445407+VitorLeiteJr@users.noreply.github.com>
2025-05-22 22:03:02 -0300

    refact: logo

--- app/components/Loading.tsx
+++ app/components/Loading.tsx
@@ -11,7 +11,7 @@ const Loading = ({status}: loadingProps) => {
   return (
     <div className='flex  bg-white justify-center items-center h-[100vh]'>
         <div  className=''>
-                <img src="https://builder.spring-builder.prod.yospace.ai/fs/userFiles-v2/segurobet-18751649/images/favicon.png?v=1709064268" 
+                <img src="#" 
                 alt="client logo" style={{ maxWidth: '200px' }} />
               <div className='flex bottom-0'>
               <h1 className='text-black'>{status}</h1> 
